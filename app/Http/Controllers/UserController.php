12f19e351f777cc9184c45efe89601b82e34955e
ccharfu <carlosuefagh@gmail.com>
2022-10-07 16:19:27 -0500

    change in the controller UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,39 +14,40 @@ class UserController extends Controller
 
     public function show($id)
     {
-        $the_permission = User::find($id);
-        if (is_null($the_permission)) {
+        $the_User = User::find($id);
+        if (is_null($the_User)) {
             return response()->json(['message' => 'Not found'], 404);
         } else {
-            return response()->json($the_permission, 200);
+            $the_User->profile;
+            return response()->json($the_User, 200);
         }
     }
 
     public function store(Request $request)
     {
-        $the_permission = User::create($request->all());
-        return response($the_permission, 201);
+        $the_User = User::create($request->all());
+        return response($the_User, 201);
     }
 
     public function update(Request $request, $id)
     {
-        $the_permission = User::find($id);
-        if (is_null($the_permission)) {
+        $the_User = User::find($id);
+        if (is_null($the_User)) {
             return response()->json(['message' => 'Not found'], 404);
         } else {
-            $the_permission->update($request->all());
-            return response()->json($the_permission, 200);
+            $the_User->update($request->all());
+            return response()->json($the_User, 200);
         }
     }
 
     public function destroy(Request $request, $id)
     {
-        $the_permission = User::find($id);
+        $the_User = User::find($id);
 
-        if (is_null($the_permission)) {
+        if (is_null($the_User)) {
             return response()->json(['message' => 'Not found'], 404);
         } else {
-            $the_permission->delete();
+            $the_User->delete();
             return response()->json(null, 204);
         }
     }
